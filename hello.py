97fc6eb8b7d44a8aa4db3faa3fe191a3987938d4
Fabio Teixeira <53839583+fabio-teixeira-ifsp@users.noreply.github.com>
2024-09-26 21:36:01 -0300

    Nova rota

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<p>Alterações por meio do GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)

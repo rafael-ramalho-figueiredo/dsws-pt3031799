59f706d423a57eb7a219ee831d45cfadf92a8758
rafael-ramalho-figueiredo <rafael.figueiredo116@gmail.com>
2025-08-28 19:58:28 +0000

    Templates adicionados

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 @app.route('/')
 def hello_world():
-    return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Rafael%20Ramalho/PT3031799/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 @app.route('/user/<name>/<prontuario>/<instituto>')
-def user(name, prontuario, instituto):
+def user2(name, prontuario, instituto):
     return '<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: {}</h2><h2>Prontuário: {}</h2><h2>Instituição: {}</h2><a href="/">Voltar</a>'.format(name, prontuario, instituto)
 
 @app.route('/contextorequisicao')
@@ -27,3 +38,11 @@ def objetoresposta():
 @app.route('/redirecionamento')
 def redirecionamento():
     return redirect('https://ptb.ifsp.edu.br/', code=302)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
